57e942114fce4c47462c2fc50b353146d76ae26a
Sanjaya Panigrahi <sanjayp.panigrahi@gmail.com>
2021-06-26 18:28:26 +0530

    Assignment 8 initial code updated

---
+++ src/com/java/bootcamp/assignment/Assignment8Application.java
@@ -0,0 +1,11 @@
+package com.java.bootcamp.assignment;
+
+public class Assignment8Application {
+
+	public static void main(String[] args) {
+		Assignment8 assignment8 = new Assignment8();
+		assignment8.getNumbers();
+
+	}
+
+}
